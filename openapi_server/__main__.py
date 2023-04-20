35638344e0924fd7ecde6b2d4b0ec368c3470706
juanmagal <juanma.galmes@gmail.com>
2023-04-20 14:30:45 +0000

    Fix NSSM OpenAPI

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -21,6 +21,8 @@ def main():
 
     __tablename__ = 'resource_db'
     __tablename__ = 'service_profile_db'
+    __tablename__ = 'slice_profile_db'
+
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     app = connexion.App(__name__, specification_dir='./openapi/')
@@ -34,6 +36,9 @@ def main():
                 pythonic_params=True,strict_validation=True,validate_responses=True)
     app.add_api('openapi1.yaml',
                 arguments={'title': 'NS Provisioning MnS'},
+                pythonic_params=True)
+    app.add_api('openapi2.yaml',
+                arguments={'title': 'NSS Provisioning MnS'},
                 pythonic_params=True)
     flaskapp.config.from_mapping(
         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
